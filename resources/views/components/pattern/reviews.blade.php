97776e6258a341d18a5ef3dc0341830f6a890b7e
Nikita Malkin <demeja16@gmail.com>
2026-02-16 09:41:45 +0300

    - remove reviews block from single pattern page if there is no reviews or avg_rating is 0 - add outlines & outline colors for elements

--- resources/views/components/pattern/reviews.blade.php
+++ resources/views/components/pattern/reviews.blade.php
@@ -1,47 +1,49 @@
 @props(['pattern'])
 
-<div {{ $attributes->merge(['class' => 'pattern-reviews']) }}>
-    <div class="pattern-reviews__header">
-        <h2 class="pattern-reviews__title">
-            {{ __('pattern.reviews') }}
-        </h2>
+@if (!$pattern->reviews->isEmpty() || $pattern->avg_rating != 0)
+    <div {{ $attributes->merge(['class' => 'pattern-reviews']) }}>
+        <div class="pattern-reviews__header">
+            <h2 class="pattern-reviews__title">
+                {{ __('pattern.reviews') }}
+            </h2>
 
-        @if ($pattern->avg_rating != 0)
-            <x-rating.stars
-                class="pattern-reviews__rating-stars"
-                max="5"
-                :stars="$pattern->avg_rating"
-            />
+            @if ($pattern->avg_rating != 0)
+                <x-rating.stars
+                    class="pattern-reviews__rating-stars"
+                    max="5"
+                    :stars="$pattern->avg_rating"
+                />
+            @endif
+        </div>
+
+        @if (!$pattern->reviews->isEmpty())
+            <div class="pattern-reviews__list">
+                @foreach ($pattern->reviews as $review)
+                    <div class="pattern-reviews__review">
+                        <div class="pattern-reviews__review-reviewer">
+                            <p class="pattern-reviews__review-reviewer-name">
+                                {{ $review->reviewer_name }}
+                            </p>
+
+                            @if ($review->rating)
+                                <x-rating.stars
+                                    class="pattern-reviews__review-reviewer-rating"
+                                    max="5"
+                                    :stars="$review->rating"
+                                />
+                            @endif
+                        </div>
+
+                        <div class="pattern-reviews__review-body">
+                            {{ $review->comment }}
+                        </div>
+
+                        <div class="pattern-reviews__review-date">
+                            {{ $review->reviewed_at->format('d.m.Y H:i') }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         @endif
     </div>
-
-    @if (!$pattern->reviews->isEmpty())
-        <div class="pattern-reviews__list">
-            @foreach ($pattern->reviews as $review)
-                <div class="pattern-reviews__review">
-                    <div class="pattern-reviews__review-reviewer">
-                        <p class="pattern-reviews__review-reviewer-name">
-                            {{ $review->reviewer_name }}
-                        </p>
-
-                        @if ($review->rating)
-                            <x-rating.stars
-                                class="pattern-reviews__review-reviewer-rating"
-                                max="5"
-                                :stars="$review->rating"
-                            />
-                        @endif
-                    </div>
-
-                    <div class="pattern-reviews__review-body">
-                        {{ $review->comment }}
-                    </div>
-
-                    <div class="pattern-reviews__review-date">
-                        {{ $review->reviewed_at->format('d.m.Y H:i') }}
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    @endif
-</div>
+@endif
