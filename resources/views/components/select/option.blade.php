233b00f071938594e112e324eb2455fdcc01b9e5
Nikita Malkin <demeja16@gmail.com>
2026-02-18 16:36:38 +0300

    add option selected prop

--- resources/views/components/select/option.blade.php
+++ resources/views/components/select/option.blade.php
@@ -1,3 +1,8 @@
-<option {{ $attributes->merge(['class' => 'select__option']) }}>
+@props(['selected' => false])
+
+<option
+    {{ $attributes->merge(['class' => 'select__option']) }}
+    @selected($selected)
+>
     {{ $slot }}
 </option>
