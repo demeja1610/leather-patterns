76add4f995dc28be0f5070f04f37bdbaa99fdf16
Nikita Malkin <demeja16@gmail.com>
2026-02-20 13:22:34 +0300

    hide rating if no reviews

--- resources/views/components/pattern/reviews.blade.php
+++ resources/views/components/pattern/reviews.blade.php
@@ -1,6 +1,6 @@
 @props(['pattern'])
 
-@if (!$pattern->reviews->isEmpty() || $pattern->avg_rating != 0)
+@if (!$pattern->reviews->isEmpty())
     <div {{ $attributes->merge(['class' => 'pattern-reviews']) }}>
         <div class="pattern-reviews__header">
             <h2 class="pattern-reviews__title">
@@ -16,34 +16,32 @@
             @endif
         </div>
 
-        @if (!$pattern->reviews->isEmpty())
-            <div class="pattern-reviews__list">
-                @foreach ($pattern->reviews as $review)
-                    <div class="pattern-reviews__review">
-                        <div class="pattern-reviews__review-reviewer">
-                            <p class="pattern-reviews__review-reviewer-name">
-                                {{ $review->reviewer_name }}
-                            </p>
+        <div class="pattern-reviews__list">
+            @foreach ($pattern->reviews as $review)
+                <div class="pattern-reviews__review">
+                    <div class="pattern-reviews__review-reviewer">
+                        <p class="pattern-reviews__review-reviewer-name">
+                            {{ $review->reviewer_name }}
+                        </p>
 
-                            @if ($review->rating)
-                                <x-rating.stars
-                                    class="pattern-reviews__review-reviewer-rating"
-                                    max="5"
-                                    :stars="$review->rating"
-                                />
-                            @endif
-                        </div>
-
-                        <div class="pattern-reviews__review-body">
-                            {{ $review->comment }}
-                        </div>
-
-                        <div class="pattern-reviews__review-date">
-                            {{ $review->reviewed_at->format('d.m.Y H:i') }}
-                        </div>
+                        @if ($review->rating)
+                            <x-rating.stars
+                                class="pattern-reviews__review-reviewer-rating"
+                                max="5"
+                                :stars="$review->rating"
+                            />
+                        @endif
                     </div>
-                @endforeach
-            </div>
-        @endif
+
+                    <div class="pattern-reviews__review-body">
+                        {{ $review->comment }}
+                    </div>
+
+                    <div class="pattern-reviews__review-date">
+                        {{ $review->reviewed_at->format('d.m.Y H:i') }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endif
