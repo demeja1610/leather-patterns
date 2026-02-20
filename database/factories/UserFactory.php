3ae61c4145d8462dc0ec92395cbc236ed18cabbd
Nikita Malkin <demeja16@gmail.com>
2026-02-20 11:27:58 +0300

    rector run (factories)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Support\Str;
@@ -23,7 +25,7 @@ class UserFactory extends Factory
 
     public function unverified(): static
     {
-        return $this->state(fn(array $attributes) => [
+        return $this->state(fn(array $attributes): array => [
             'email_verified_at' => null,
         ]);
     }
