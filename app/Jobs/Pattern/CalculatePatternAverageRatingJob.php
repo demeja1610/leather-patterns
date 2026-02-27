7bda6fba6dadb9a54e0a5254ccd7edb70f7655b8
Nikita Malkin <demeja16@gmail.com>
2026-02-27 10:18:40 +0300

    refactor pattern average rating calculating job

--- app/Jobs/Pattern/CalculatePatternAverageRatingJob.php
+++ app/Jobs/Pattern/CalculatePatternAverageRatingJob.php
@@ -13,48 +13,78 @@ class CalculatePatternAverageRatingJob implements ShouldQueue
 {
     use Queueable;
 
+    protected string $actionName = 'calculate pattern(s) average rating';
+
     public function __construct(
         public ?int $patternId = null
     ) {}
 
     public function handle(): void
     {
-        Log::info('Start calculating patterns average rating');
+        Log::info("Start {$this->actionName}");
 
         $q = $this->getBaseQuery();
 
         if ($this->patternId !== null) {
+            Log::info(ucfirst($this->actionName) . ". Patterd ID: {$this->patternId}");
+
             $q->where('patterns.id', $this->patternId);
+
+            $pattern = $q->first();
+
+            $updated = 0;
+
+            if ($pattern === null) {
+                Log::info(ucfirst($this->actionName) . ". Specified pattern with ID: {$this->patternId} don't have any reviews or don't exists");
+
+                Log::info(ucfirst($this->actionName) . ". If pattern with ID: {$this->patternId} exists it's average rating will be set to zero");
+
+                $updated =  DB::table('patterns')->where('id', $this->patternId)->update([
+                    'avg_rating' => 0,
+                ]);
+            } else {
+                $updated =  DB::table('patterns')->where('id', $this->patternId)->update([
+                    'avg_rating' => $pattern->avg_rating,
+                ]);
+            }
+
+            if ($updated > 0) {
+                Log::info(ucfirst($this->actionName) . ". Average rating for pattern with ID: {$this->patternId} was updated");
+            } else {
+                Log::info(ucfirst($this->actionName) . ". No pattern was updated");
+            }
+        } else {
+            $i = 1;
+
+            $q->chunkById(
+                count: 250,
+                callback: function (Collection $patterns) use (&$i): void {
+                    $case = 'CASE';
+                    $ids = [];
+                    $count = $patterns->count();
+
+                    Log::info(ucfirst($this->actionName) . ", processing chunk: {$i} containing: {$count} patterns");
+
+                    foreach ($patterns as $pattern) {
+                        $case .= " WHEN id = {$pattern->id} THEN {$pattern->avg_rating}";
+
+                        $ids[] = $pattern->id;
+                    }
+
+                    $case .= ' ELSE avg_rating END';
+
+                    DB::table('patterns')->whereIn(column: 'id', values: $ids)->update(values: [
+                        'avg_rating' => DB::raw($case),
+                    ]);
+
+                    $i++;
+                },
+                column: 'patterns.id',
+                alias: 'id',
+            );
         }
 
-        $i = 1;
-
-        $q->chunkById(
-            count: 250,
-            callback: function (Collection $patterns) use (&$i): void {
-                $case = 'CASE';
-                $ids = [];
-                $count = $patterns->count();
-
-                Log::info("Calculating patterns average rating, processing chunk: {$i} containing: {$count} patterns");
-
-                foreach ($patterns as $pattern) {
-                    $case .= " WHEN id = {$pattern->id} THEN {$pattern->avg_rating}";
-
-                    $ids[] = $pattern->id;
-                }
-
-                $case .= ' ELSE avg_rating END';
-
-                DB::table('patterns')->whereIn(column: 'id', values: $ids)->update(values: [
-                    'avg_rating' => DB::raw($case),
-                ]);
-
-                $i++;
-            },
-            column: 'patterns.id',
-            alias: 'id',
-        );
+        Log::info("Finish {$this->actionName}");
     }
 
     protected function getBaseQuery(): Builder
