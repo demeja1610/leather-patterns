95fe672ba89fe301101967ffc3fd657154cd2c98
Nikita Malkin <demeja16@gmail.com>
2026-03-05 10:19:35 +0300

    add schedule to calculate pattern avg rating

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -47,5 +47,8 @@ return Application::configure(basePath: dirname(path: __DIR__))
 
         $schedule->job(new \App\Jobs\Pattern\ReplaceMarkedForReplacePatternTagsInPatternsJob)
             ->dailyAt('23:59:59');
+
+        $schedule->job(new \App\Jobs\Pattern\CalculatePatternAverageRatingJob)
+            ->dailyAt('23:59:59');
     })
     ->withExceptions(using: function (Exceptions $exceptions): void {})->create();
