2bcc45c9e6ef66404ec4856049bed1d289ebe6ed
Nikita Malkin <demeja16@gmail.com>
2026-03-24 16:14:46 +0300

    remove legacy

--- config/parse_sources.php
+++ config/parse_sources.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 use App\Enum\PatternSourceEnum;
 
 return [
-    // PatternSourceEnum::LEATHER_PATTERNS->value => 'https://leatherpatterns.ru',
-    // PatternSourceEnum::CUTME->value => 'https://cutme.info',
-    // PatternSourceEnum::V_POMOSH_KOZHEVNIKU->value => 'https://patterns.somstylecraft.ru',
-    // PatternSourceEnum::FORMULA_KOZHI->value => 'https://formulakozhi.ru',
-    // PatternSourceEnum::PATTERN_HUB->value => 'https://patternhub.su',
-    // PatternSourceEnum::PABLIK_KOZHEVNIKA->value => 'https://pablik-kozhevnika.ru',
-    // PatternSourceEnum::SKINPAT->value => 'https://skinpat.ru',
-    // PatternSourceEnum::FIRST_KOZHA->value => 'https://1koja.ru',
-    // PatternSourceEnum::MYETSY->value => 'https://myetsy.ru', // At the refator time (24.03.2026) site is disabled by hosting owners due to unpaid bills
-    // PatternSourceEnum::LASERBIZ->value => 'https://maket.laserbiz.ru',
-    // PatternSourceEnum::ABZALA->value => 'https://abzala.com',
+    PatternSourceEnum::LEATHER_PATTERNS->value => 'https://leatherpatterns.ru',
+    PatternSourceEnum::CUTME->value => 'https://cutme.info',
+    PatternSourceEnum::V_POMOSH_KOZHEVNIKU->value => 'https://patterns.somstylecraft.ru',
+    PatternSourceEnum::FORMULA_KOZHI->value => 'https://formulakozhi.ru',
+    PatternSourceEnum::PATTERN_HUB->value => 'https://patternhub.su',
+    PatternSourceEnum::PABLIK_KOZHEVNIKA->value => 'https://pablik-kozhevnika.ru',
+    PatternSourceEnum::SKINPAT->value => 'https://skinpat.ru',
+    PatternSourceEnum::FIRST_KOZHA->value => 'https://1koja.ru',
+    PatternSourceEnum::MYETSY->value => 'https://myetsy.ru', // At the refator time (24.03.2026) site is disabled by hosting owners due to unpaid bills
+    PatternSourceEnum::LASERBIZ->value => 'https://maket.laserbiz.ru',
+    PatternSourceEnum::ABZALA->value => 'https://abzala.com',
     PatternSourceEnum::NEOVIMA->value => 'https://neovima.com',
-
-
-    // PatternSourceEnum::MLEATHER->value => 'https://www.mleather.ru',
-    // // PatternSourceEnum::SKINCUTS->value => 'https://skincuts.ru',
+    // PatternSourceEnum::SKINCUTS->value => 'https://skincuts.ru', // Contains too much duplicate patterns, skip untill we can handle duplicates
 ];
