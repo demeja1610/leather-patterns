77fb4fd75363af3dfb254eb49cf107cb265bbcd4
Nikita Malkin <demeja16@gmail.com>
2026-04-20 15:12:01 +0300

    - update laravel & packages - add spatie/image package - add image optimization whille upload

--- app/Http/Controllers/Admin/PatternImage/Action/UploadController.php
+++ app/Http/Controllers/Admin/PatternImage/Action/UploadController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin\PatternImage\Action;
 
+use Spatie\Image\Image;
+use Illuminate\Support\Str;
 use App\Models\PatternImage;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Admin\PatternImage\UploadRequest;
 
 class UploadController extends Controller
@@ -11,18 +15,69 @@ class UploadController extends Controller
     public function __invoke(UploadRequest $request)
     {
         $saved = [];
+        $newPatternImage = new PatternImage();
+        $disk = $newPatternImage->getSaveToDiskName();
+        $uploadPath = $newPatternImage->getUploadPath();
 
         if ($request->hasFile('images')) {
-            $newPatternImage = new PatternImage();
+            foreach ($request->file('images') as $imageFile) {
+                $filename = Str::uuid() . '.jpg';
+                $tempPath = $imageFile->getRealPath();
 
-            /**
-             * @var \Illuminate\Http\UploadedFile $imagefile
-             */
-            foreach ($request->file('images') as $imagefile) {
-                $path = $imagefile->store($newPatternImage->getUploadPath(), $newPatternImage->getSaveToDiskName());
+                $tempImage = Image::load($tempPath);
+                $originalWidth = $tempImage->getWidth();
+                $originalSize = $imageFile->getSize();
 
-                if ($path !== false) {
-                    $saved[] = asset('storage/' . $path); // TODO: private storages?
+                $targetWidth = 1920;
+                $shouldResize = $originalWidth > $targetWidth;
+
+                $optimizedTempPath = tempnam(sys_get_temp_dir(), 'optimized_');
+
+                try {
+                    $image = Image::load($tempPath);
+
+                    if ($shouldResize) {
+                        $image->width($targetWidth);
+                    }
+
+                    $image->quality(80)
+                        ->format('jpg')
+                        ->save($optimizedTempPath);
+
+                    $optimizedSize = filesize($optimizedTempPath);
+
+                    $finalTempPath = $optimizedTempPath;
+                    $usedOptimized = true;
+
+                    if ($optimizedSize > $originalSize) {
+                        $finalTempPath = $tempPath;
+                        $usedOptimized = false;
+
+                        Log::info("Optimized image larger than original", [
+                            'original' => $originalSize,
+                            'optimized' => $optimizedSize,
+                            'filename' => $imageFile->getClientOriginalName()
+                        ]);
+                    }
+
+                    $savedPath = Storage::disk($disk)->putFileAs($uploadPath, $finalTempPath, $filename);
+
+                    if ($usedOptimized && file_exists($optimizedTempPath)) {
+                        unlink($optimizedTempPath);
+                    }
+
+                    if ($savedPath) {
+                        $saved[] = Storage::disk($disk)->url($savedPath);
+                    }
+                } catch (\Exception $e) {
+                    if (file_exists($optimizedTempPath)) {
+                        unlink($optimizedTempPath);
+                    }
+
+                    Log::error("Image optimization failed", [
+                        'error' => $e->getMessage(),
+                        'filename' => $imageFile->getClientOriginalName()
+                    ]);
                 }
             }
         }
